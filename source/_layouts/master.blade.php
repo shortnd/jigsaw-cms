f6f1a68273b753396cecf885af72f181ab007bfc
shortnd <ococncol@gmail.com>
2018-08-07 11:03:47 -0400

    last attempt before trying again later

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -10,15 +10,15 @@
     <body>
         @yield('body')
         <script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-</script>
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        </script>
     </body>
 </html>
