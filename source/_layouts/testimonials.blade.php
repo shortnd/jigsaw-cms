a446581cf3ab1db2e0bf11f3d7ff723f80ba13e1
shortnd <ococncol@gmail.com>
2018-08-14 00:03:42 -0400

    change to list

--- source/_layouts/testimonials.blade.php
+++ source/_layouts/testimonials.blade.php
@@ -3,4 +3,10 @@
   <blockquote>
     {{ $page->quote}}
   </blockquote>
+  {{-- @foreach ($page->author as $author)
+    <p> {{$author}} </p>
+  @endforeach --}}
+  <div class="" style="background:green">
+    {{$page->$author[1]}}
+  </div>
 @endsection
