9fc3712b40f9abd62079f458826ffefc88252aa7
shortnd <ococncol@gmail.com>
2018-08-14 18:55:48 -0400

    pulling

--- source/_layouts/testimonials.blade.php
+++ source/_layouts/testimonials.blade.php
@@ -3,10 +3,4 @@
   <blockquote>
     {{ $page->quote}}
   </blockquote>
-  {{-- @foreach ($page->author as $author)
-    <p> {{$author}} </p>
-  @endforeach --}}
-  <div class="" style="background:green">
-    {{$page->$author[1]}}
-  </div>
 @endsection
